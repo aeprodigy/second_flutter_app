ced72ed436592b783b77259c392ca41775ccf5a4
mike mambwe <mikeapple056@gmail.com>
2023-03-01 13:36:56 +0200

    title:

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ void main()=> runApp(MaterialApp(
   
   home: Scaffold(
     appBar: AppBar(
-      title: const Text('Flutter Lesson 100 Days'),
+      title: const Text('Flutter Lesson 1 of 100 Days'),
       centerTitle: true,
+      backgroundColor: Colors.lime,
     ),
     body: const Center(child: Text('Flutter app')
     ),
